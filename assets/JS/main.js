b5c9fa5ef26ad091f2f27f5e1e9fd3ac0c59256d
JulianBenchimol <jbenchimol1999@gmail.com>
2022-11-04 14:08:59 -0400

    initial push

---
+++ assets/JS/main.js
@@ -0,0 +1,15 @@
+var edenModelButton = $("#eden-model-button");
+var oasisModelButton = $("#oasis-model-button");
+var mirageModelButton = $("#mirage-model-button");
+
+edenModelButton.on('click', function(){
+    document.location = "https://www.waterlodge.co.uk/waterlodge-range/waterlodge-eden/";
+});
+
+oasisModelButton.on('click', function(){
+    document.location = 'https://www.waterlodge.co.uk/waterlodge-range/waterlodge-oasis/'
+});
+
+mirageModelButton.on('click', function(){
+    document.location = 'https://www.waterlodge.co.uk/waterlodge-range/waterlodge-mirage/';
+});
