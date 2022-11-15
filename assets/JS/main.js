a902ccb67f110add1b64150c14ebf344b0e4f815
JulianBenchimol <jbenchimol1999@gmail.com>
2022-11-15 12:46:57 -0500

    learn more btns access data for dynamic page view

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,10 +1,24 @@
+function Model (name, price, length, height, heightWater, draft){
+    this.modelName = name;
+    this.price = price;
+    this.length = length;
+    this.height = height;
+    this.heightWater = heightWater;
+    this.draft = draft;
+}
+
 var modernModelButton = $('#modern-model-button');
 var apartmentModelButton = $('#apartment-model-button');
+var learnMoreButtons = $('[id = "learn-more"]');
 
 modernModelButton.on('click', function(){
     document.location = './modern-models.html';
 });
-
 apartmentModelButton.on('click', function(){
     document.location = './apartment-models.html';
 });
+
+learnMoreButtons.on('click', function(event){
+    const model = event.target.getAttribute('data-model');
+    console.log(model);
+})
